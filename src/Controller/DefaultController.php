d42cd65480f8ea963e335c82887f2733b7066763
ib3g <i.barry3g@gmail.com>
2023-04-10 10:24:58 +0000

    init front template

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the Chellal project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Developed by Monarkit
+ *
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(){
+        return $this->render('home.html.twig');
+    }
+}
